a1be4820c01325a058ca25b9a057c7977e0c70a7
[dyad] <allexszla@gmail.com>
2026-06-05 22:10:58 -0300

    Apply react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-"use client";
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FinanceProvider } from "./context/FinanceContext";
-import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
-import TransactionsPage from "./pages/Transactions";
-import AccountsPage from "./pages/Accounts";
-import CardsPage from "./pages/Cards";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <FinanceProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/transactions" element={<TransactionsPage />} />
-              <Route path="/accounts" element={<AccountsPage />} />
-              <Route path="/cards" element={<CardsPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </FinanceProvider>
-    </ThemeProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
